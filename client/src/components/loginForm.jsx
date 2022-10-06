c5b704ffcdbc3b1d4963d6aa7fce68a2079f6fc9
Karla Abad <kabad1014@gmail.com>
2022-10-06 14:01:44 -0400

    Finish CSS Login Form.

--- client/src/components/loginForm.jsx
+++ client/src/components/loginForm.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import logo from "../images/logo-ANCLA.png";
+import { ReactComponent as Instagram } from "../images/instagram.svg";
+import { ReactComponent as Facebook } from "../images/facebook.svg";
+import { ReactComponent as Twitter } from "../images/twitter.svg";
+import { ReactComponent as Youtube } from "../images/youtube.svg";
 
 const LoginForm = () => {
   return (
@@ -7,7 +11,7 @@ const LoginForm = () => {
       <div className="flex">
         <div>
           <img src={logo} />
-          <p>
+          <p className="main--description">
             Keep track of enrolled students and teachers for the current school
             year.
           </p>
@@ -20,7 +24,19 @@ const LoginForm = () => {
           <button className="btn btn--green">Register</button>
         </form>
       </div>
-      <div className="login__foter">General Information Footer</div>
+      <div className="login__footer">
+        <p className="login__footer-text">
+          Unidad Educativa Academia Naval Cap. Leonardo Abad A.
+        </p>
+        <p className="login__footer-text">Calle 19 y Avenida Malecon</p>
+        <p className="login__footer-text">La Libertad, Santa Elena - Ecuador</p>
+        <p className="login__footer-text">Telf: (04) 2777- 4875</p>
+        <hr />
+        <Instagram />
+        <Facebook />
+        <Twitter />
+        <Youtube />
+      </div>
     </div>
   );
 };
