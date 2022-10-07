dd4b62f1aca7b41f4b5df5805588ba8a131a70d5
Karla Abad <kabad1014@gmail.com>
2022-10-07 15:43:04 -0400

    Build Navbar css.

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,9 +1,20 @@
+import logo from "../images/logo-ANCLA.png";
 import { Outlet } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      NAVIGATION BAR GOES HERE!!
+      <nav>
+        <div className="nav__menu-left">
+          <img className="nav__logo" src={logo} alt="School Logo" />
+          <NavLink aria-current="page" to="/teachers">
+            Teachers
+          </NavLink>
+          <NavLink to="/students">Students</NavLink>
+        </div>
+        <button className="btn navbar__logout">Logout</button>
+      </nav>
       <Outlet />
     </div>
   );
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,27 +5,27 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: [true, "Username is required"],
-      minlength: [5, "Username must be at least 5 characters long"],
+      required: [true, "Username is required."],
+      minlength: [5, "Username must be at least 5 characters long."],
     },
     firstName: {
       type: String,
-      required: [true, "First Name is required"],
-      minlength: [3, "First Name must be at least 3 characters long"],
+      required: [true, "First Name is required."],
+      minlength: [3, "First Name must be at least 3 characters long."],
     },
     lastName: {
       type: String,
-      required: [true, "Last Name is required"],
-      minlength: [3, "Last Name must be at least 3 characters long"],
+      required: [true, "Last Name is required."],
+      minlength: [3, "Last Name must be at least 3 characters long."],
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
+      required: [true, "Email is required."],
     },
     password: {
       type: String,
-      required: [true, "Password is required"],
-      minlength: [6, "Password must be at least 6 characters long"],
+      required: [true, "Password is required."],
+      minlength: [6, "Password must be at least 6 characters long."],
     },
   },
   { timestamps: true }
