360e3d6d6930a3478148151ac2f280c9cebf8246
Karla Abad <kabad1014@gmail.com>
2022-10-10 08:54:09 -0400

    Read teachers from MongoDB.

--- client/src/components/teachers.jsx
+++ client/src/components/teachers.jsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Teachers = () => {
+  const [teachers, setTeachers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/teachers")
+      .then((res) => {
+        console.log(res.data.allTeachers);
+        setTeachers(res.data.allTeachers);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>TEACHERS HERE!!</h1>
-    </div>
+    <section>
+      <button className="btn btn--green">New Teacher</button>
+      <table>
+        <thead>
+          <tr>
+            <th>First</th>
+            <th>Middle</th>
+            <th>Last</th>
+            <th>Section</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teachers.map((teacher) => (
+            <tr key={teacher._id}>
+              <td>{teacher.firstName}</td>
+              <td>{teacher.middleName}</td>
+              <td>{teacher.lastName}</td>
+              <td>{teacher.section}</td>
+              <td>
+                <button className="btn btn--blue btn--table">Update</button>
+                <button className="btn btn--red btn--table">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
 };
 
